194f0bac3c55c8930d9da0acd4b5b67b1037e6f1
Kang <294524708@qq.com>
2016-10-23 23:52:38 +0800

    json抽象类

---
+++ src/cn/result/JsonResultAbstract.java
@@ -0,0 +1,15 @@
+package cn.result;
+
+import cn.result.info.JsonResultInfo;
+
+/**
+ * Created by yan on 2016/10/22.
+ */
+public abstract class JsonResultAbstract {
+
+    private JsonResultInfo jsonResultInfo;
+
+    public JsonResultInfo getzResultInfo() {
+        return jsonResultInfo;
+    }
+}
