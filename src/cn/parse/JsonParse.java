61a9f0c10f845eaf311ec6b71bd1c1e95dd8739d
Kang <294524708@qq.com>
2016-10-23 21:24:31 +0800

    json解析器

---
+++ src/cn/parse/JsonParse.java
@@ -0,0 +1,48 @@
+package cn.parse;
+
+import cn.result.JsonResult;
+import cn.result.impl.JsonResultImpl;
+import cn.result.info.JsonResultInfo;
+import com.company.JsonValue;
+
+/**
+ * Created by yan on 2016/10/22.
+ */
+public class JsonParse {
+
+    private JsonResultInfo jResultInfo;
+
+    private String json;
+
+    public JsonParse(String json) {
+        this.json = json;
+    }
+
+
+    public JsonResult fromJson() {
+        if(json==null)
+            throw new RuntimeException("not value");
+        char[] ch = json.toCharArray();
+        if(ch[0]!= JsonValue.jsonListBegin.value()&&ch[0]!=JsonValue.jsonMapBegin.value()){
+            throw new RuntimeException("not value");
+        }
+        JsonInfo JInfo = new JsonInfo();
+        JsonResultImpl jResult = new JsonResultImpl();
+        jResultInfo = jResult.getzResultInfo();
+        StringBuffer sb = new StringBuffer();
+        for(int i=0;i<ch.length;i++){
+            char temp = ch[i];
+            if(!JInfo.isMark()&&temp == JsonValue.jsonListBegin.value()){
+                //
+                continue;
+            }else if(!JInfo.isMark() && temp == JsonValue.jsonMapBegin.value()){
+                //
+                continue;
+            }else if(temp == '\\'){
+                //
+                continue;
+            }
+        }
+        return jResult;
+    }
+}
---
+++ src/cn/result/info/JsonResultInfo.java
@@ -0,0 +1,7 @@
+package cn.result.info;
+
+/**
+ * Created by yan on 2016/10/22.
+ */
+public class JsonResultInfo {
+}
